693d8e1e2c68e9b9e58604712f2f6e2f61076378
Jonathan <unirz@student.kit.edu>
2024-07-25 10:31:33 +0200

    Added test for FileReader and DayWeather

--- src/test/java/de/exxcellent/challenge/WeatherDataTest.java
+++ src/test/java/de/exxcellent/challenge/WeatherDataTest.java
@@ -4,13 +4,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 /**
  * Example JUnit 5 test case.
  * @author Benjamin Schmid <benjamin.schmid@exxcellent.de>
  */
-class AppTest {
-
+class WeatherDataTest {
     private String successLabel = "not successful";
 
     @BeforeEach
@@ -22,6 +22,27 @@ class AppTest {
     void aPointlessTest() {
         assertEquals("successful", successLabel, "My expectations were not met");
     }
+
+    @Test
+    void getTemperatureSpread() {
+        DayWeather day = new DayWeather(1, 3, 5);
+        assertEquals(day.getTemperaturSpread(), 2);
+    }
+
+
+    @Test
+    void readWeatherCSV() {
+        String testFile = "src/main/resources/de/exxcellent/challenge/weather.csv";
+
+        assertDoesNotThrow(() -> {
+            WeatherDataReader reader = new WeatherDataReader();
+            List<DayWeather> weatherData = reader.readWeatherData(testFile);
+        });
+    }
+
+
+
+
 
     @Test
     void runFootball() {
