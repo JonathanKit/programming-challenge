e4f7b84dddcae9a361f27a901ef949c0fb315b7a
Joni <51087223+JonathanKit@users.noreply.github.com>
2024-07-26 10:07:49 +0200

    Updated test after refactoring and added football data tests

---
+++ src/test/java/de/exxcellent/challenge/DataTest.java
@@ -0,0 +1,106 @@
+package de.exxcellent.challenge;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Example JUnit 5 test case.
+ * @author Benjamin Schmid <benjamin.schmid@exxcellent.de>
+ */
+class DataTest {
+    private String successLabel = "not successful";
+
+    @BeforeEach
+    void setUp() {
+        successLabel = "successful";
+    }
+
+    @Test
+    void aPointlessTest() {
+        assertEquals("successful", successLabel, "My expectations were not met");
+    }
+
+    @Test
+    void getTemperatureSpread() {
+        DayWeather day = new DayWeather("1", 5, 3);
+        assertEquals(day.getTemperatureSpread(), 2);
+    }
+
+    @Test
+    void readWeatherCSV() {
+        assertDoesNotThrow(() -> {
+            FileReaderCsv<DayWeather> weatherReader = new FileReaderCsv<>(values -> new DayWeather(
+                    values[0],
+                    Integer.parseInt(values[1]),
+                    Integer.parseInt(values[2])
+            ));
+            List<DayWeather> weatherData = weatherReader.readData("src/main/resources/de/exxcellent/challenge/weather.csv");
+
+            assertNotNull(weatherData);
+            assertNotEquals(0, weatherData.size());
+        });
+    }
+
+    @Test
+    void findDayWithSmallestSpread() {
+        final String testFile = "src/main/resources/de/exxcellent/challenge/weather.csv";
+        WeatherAnalyzer weatherAnalyzer = new WeatherAnalyzer(testFile);
+        String dayWithSmallestTempSpread = weatherAnalyzer.findDayWithSmallestSpread();
+        assertNotEquals(dayWithSmallestTempSpread, "", "-1 means an error occurred");
+        assertEquals("14", dayWithSmallestTempSpread, "In the testfile day 14 has the lowest spread of temperature.");
+    }
+
+    @Test
+    void findDayWithSmallestSpreadWrongCsv() {
+        final String testFileWrongCsv = "src/main/resources/de/exxcellent/challenge/weather_wrong_csv.csv";
+        WeatherAnalyzer weatherAnalyzer = new WeatherAnalyzer(testFileWrongCsv);
+        String dayWithSmallestTempSpread;
+        try {
+            dayWithSmallestTempSpread = weatherAnalyzer.findDayWithSmallestSpread();
+        } catch (Exception e) {
+            dayWithSmallestTempSpread = "";
+        }
+        assertEquals("", dayWithSmallestTempSpread, "The testfile with an error in the csv file should fail.");
+    }
+
+
+
+
+    @Test
+    void runFootball() {
+        App.main("--football", "football.csv");
+    }
+
+    @Test
+    void readFootballCSV() {
+        assertDoesNotThrow(() -> {
+            FileReaderCsv<FootballRecord> footballReader = new FileReaderCsv<>(values -> new FootballRecord(
+                    values[0],
+                    Integer.parseInt(values[1]),
+                    Integer.parseInt(values[2])
+            ));
+            List<FootballRecord> footballData = footballReader.readData("src/main/resources/de/exxcellent/challenge/football.csv");
+
+            assertNotNull(footballData);
+            assertNotEquals(0, footballData.size());
+        });
+    }
+
+    @Test
+    void findFootballRecordWithSmallestSpread() {
+        String testFile = "src/main/resources/de/exxcellent/challenge/football.csv";
+        WeatherAnalyzer weatherAnalyzer = new WeatherAnalyzer(testFile);
+        String recordWithSmallestTempSpread;
+        try {
+            recordWithSmallestTempSpread = weatherAnalyzer.findDayWithSmallestSpread();
+        } catch (Exception e) {
+            recordWithSmallestTempSpread = "";
+        }
+        assertEquals("Arsenal", recordWithSmallestTempSpread, "In the testfile Arsenal has the lowest spread of goals.");
+    }
+
+}
